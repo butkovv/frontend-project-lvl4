a48564b6aa07e078177d8b93409ca19283eb7061
Vyacheslav Butko <butkovv1991@gmail.com>
2020-10-28 18:06:15 +0300

    move rollbar to index.pug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import Rollbar from 'rollbar';
+// import Rollbar from 'rollbar';
 import Channels from './Channels.jsx';
 import Messages from './Messages.jsx';
 import MessageBox from './MessageBox.jsx';
 
-const App = () => {
-  React.useState({
-    rollbar: new Rollbar({
-      enabled: process.env.NODE_ENV === 'production',
-      accessToken: '2fb1743bfd9645ef9fb59b0c29e54db3',
-      captureUncaught: true,
-      captureUnhandledRejections: true,
-    }),
-  });
-
-  return (
-    <div className="row h-100 pb-3">
-      <Channels />
-      <div className="col h-100">
-        <div className="d-flex flex-column h-100">
-          <Messages />
-          <MessageBox />
-        </div>
+const App = () => (
+  <div className="row h-100 pb-3">
+    <Channels />
+    <div className="col h-100">
+      <div className="d-flex flex-column h-100">
+        <Messages />
+        <MessageBox />
       </div>
     </div>
-  );
-};
+  </div>
+);
 export default App;
